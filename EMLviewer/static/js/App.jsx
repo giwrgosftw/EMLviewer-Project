9b60a82b8bbccedc02b4bf059b1232e588362fa4
George Karanasios <kar.giorgos7@hotmail.com>
2020-04-19 17:34:41 +0300

    Application files

---
+++ EMLviewer/static/js/App.jsx
@@ -0,0 +1,37 @@
+// App.jsx - is connected with Index.jsx (OUTPUT POINT)
+// Interacts with the css folder, background image and Main.jsx
+
+import React from "react";
+import Main from "./Main";
+import { PageHeader } from "react-bootstrap";
+
+require('../css/fullstack.css');
+var $ = require('jquery'); // Adding JQuery library/requirements for HTTP requests
+
+import HeaderBackgroundImage from '../images/header.jpg';
+
+// Always have to import+export the react files in order to show what they do
+export default class App extends React.Component {
+	constructor(props) {
+		super(props); // props are parameters that passed into the constructor
+		              // are public and re-rendered the UI after any update state
+	}
+
+	// Insert the background image
+	addHeaderImg() {
+		let headerBg = new Image();
+		headerBg.src = HeaderBackgroundImage;
+	}
+
+    // Display background image and Main.jsx content
+	render () {
+        return (
+			<PageHeader>
+				<div className='header-contents'>
+				{this.addHeaderImg()}
+				<Main />
+				</div>
+			</PageHeader>
+		);
+    }
+}
