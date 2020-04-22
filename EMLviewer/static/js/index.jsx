b5a051cad210ca66d72ad045316452283e139709
George Karanasios <kar.giorgos7@hotmail.com>
2020-04-22 15:21:29 +0300

    Application files

--- EMLviewer/static/js/index.jsx
+++
@@ -1,8 +0,0 @@
-// Index.jsx - Our Front-end environment (ENTRY POINT)
-// Is connected with index.html (keyword: content)
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("content"));
